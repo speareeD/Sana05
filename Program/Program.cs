8e1da6da64a3c4abbbc1f1701aeb64287843f2d6
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2026-01-09 15:47:38 +0200

    Created vehicles array with 3 instances of different inherited classes inside

--- Program/Program.cs
+++ Program/Program.cs
@@ -4,6 +4,12 @@
     {
         public static void Main(string[] args)
         {
+            Vehicle[] vehicles =
+            {
+                new Car("Toyota", "Corolla", 2020, 25000, 4, "gasoline"),
+                new Truck("Volvo", "FH16", 2021, 120000, 25, 4),
+                new Motorcycle("Harley-Davidson", "Street Glide", 2022, 28000, 1746, true)
+            };
 
         }
     }
