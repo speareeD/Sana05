bafef9b2c68bd62b1291fd15acdeb35c9bdf7e09
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2026-01-09 14:51:30 +0200

    Created class vehicle and added fields and constructor

---
+++ Program/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿namespace Program
+{
+    public class Vehicle
+    {
+        protected string Brand;
+        protected string Model;
+        protected int Year;
+        protected double Price;
+
+        public Vehicle(string brand, string model, int year, double price)
+        {
+            Brand = brand;
+            Model = model;
+            Year = year;
+            Price = price;
+        }
+
+    }
+}
