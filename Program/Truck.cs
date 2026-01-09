9a0b5a5dd3dbcb6a528ecf32b1fcd901a9d66c3c
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2026-01-09 15:12:50 +0200

    Created inherited class of Vehicle, added new fields and constructor

---
+++ Program/Truck.cs
@@ -0,0 +1,17 @@
+﻿namespace Program
+{
+    public class Truck : Vehicle
+    {
+        protected double LoadCapacity;
+        protected int NumberOfAxles;
+
+        public Truck(string brand, string model, int year, double price, double loadCapacity, int numberOfAxles)
+            : base(brand, model, year, price)
+        {
+            LoadCapacity = loadCapacity;
+            NumberOfAxles = numberOfAxles;
+        }
+
+
+    }
+}
