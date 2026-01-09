8ad363c1d9fde66739971310c8844535194760ab
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2026-01-09 14:58:57 +0200

    Created instance of Vehicle class named Car, added own fields and created constructor

---
+++ Program/Car.cs
@@ -0,0 +1,17 @@
+﻿namespace Program
+{
+    public class Car : Vehicle
+    {
+        protected int NumberOfDoors;
+        protected string FuelType;
+
+        public Car(string brand, string model, int year, double price, int numberOfDoors, string fuelType)
+            : base(brand, model, year, price)
+        {
+            NumberOfDoors = numberOfDoors;
+            FuelType = fuelType;
+        }
+
+
+    }
+}
