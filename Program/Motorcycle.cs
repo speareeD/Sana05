0d8bdc383256a8eb412a727756013cb85eadd2bf
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2026-01-09 15:22:02 +0200

    Created inherited class of Vehicle named Motorcycle. Added there fields and created constructor

---
+++ Program/Motorcycle.cs
@@ -0,0 +1,16 @@
+﻿namespace Program
+{
+    public class Motorcycle : Vehicle
+    {
+        protected int EngineVolume;
+        protected bool HasWindshield;
+
+        public Motorcycle(string brand, string model, int year, double price, int engineVolume, bool hasWindshield)
+            : base(brand, model, year, price)
+        {
+            EngineVolume = engineVolume;
+            HasWindshield = hasWindshield;
+        }
+
+    }
+}
